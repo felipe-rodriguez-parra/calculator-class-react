31a2dce912e57b8ceae2b324814377f0e7a790f1
Felipe Rodriguez <felipeparra@protonmail.com>
2022-08-19 13:20:58 +0200

    import data from other project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <div className='calculator-container'>
+
+        </div>
+      </header>
     </div>
   );
 }
