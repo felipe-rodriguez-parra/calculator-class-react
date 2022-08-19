86c9cd5f27bfb0e0bba6169a7c586889c61b8354
Felipe Rodriguez <felipeparra@protonmail.com>
2022-08-19 18:52:32 +0200

    Setup Screen and styles

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,5 +3,14 @@ import "../stylesheets/Screen.css";
 
 
 export class Screen extends React.Component {
-  
+
+  render() {
+    return (
+      <div className="screen-container">
+        <div className="screen-container-text">
+          {this.props.screen}
+        </div>
+      </div>
+    )
+  }
 }
