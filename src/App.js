da8702f119195d7c550b8f7eaf8118b951f4f027
Felipe Rodriguez <felipeparra@protonmail.com>
2022-08-19 18:52:50 +0200

    add state for screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from './components/Button';
+import { Screen } from './components/Screen';
 import data from './datasets/data.json';
 import './App.css';
 
@@ -7,7 +8,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: data
+      data: data,
+      screen: '0',
     };
   }
 
@@ -16,55 +18,56 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <div className='calculator-container'>
-          <div className='row'>
-              <Button 
-              name={data.slice(15, 19).map(data => {
-                return (
-                  <div className='button-container'>
-                    {data.name}
-                  </div>
-                )
-              })} />
+          <Screen screen={this.state.screen} />
+            <div className='row'>
+              <Button
+                name={data.slice(15, 19).map(data => {
+                  return (
+                    <div className='button-container'>
+                      {data.name}
+                    </div>
+                  )
+                })} />
             </div>
             <div className='row'>
-              <Button 
-              name={data.slice(0, 4).map(data => {
-                return (
-                  <div className='button-container'>
-                    {data.name}
-                  </div>
-                )
-              })} />
+              <Button
+                name={data.slice(0, 4).map(data => {
+                  return (
+                    <div className='button-container'>
+                      {data.name}
+                    </div>
+                  )
+                })} />
             </div>
             <div className='row'>
-              <Button 
-              name={data.slice(4, 8).map(data => {
-                return (
-                  <div className='button-container'>
-                    {data.name}
-                  </div>
-                )
-              })} />
+              <Button
+                name={data.slice(4, 8).map(data => {
+                  return (
+                    <div className='button-container'>
+                      {data.name}
+                    </div>
+                  )
+                })} />
             </div>
             <div className='row'>
-              <Button 
-              name={data.slice(8, 12).map(data => {
-                return (
-                  <div className='button-container'>
-                    {data.name}
-                  </div>
-                )
-              })} />
+              <Button
+                name={data.slice(8, 12).map(data => {
+                  return (
+                    <div className='button-container'>
+                      {data.name}
+                    </div>
+                  )
+                })} />
             </div>
             <div className='row'>
-              <Button 
-              name={data.slice(12, 15).map(data => {
-                return (
-                  <div className='button-container'>
-                    {data.name}
-                  </div>
-                )
-              })} />
+              <Button
+                name={data.slice(12, 15).map(data => {
+                  return (
+                    <div className='button-container'>
+                      {data.name}
+                    </div>
+                  )
+                })} />
             </div>
           </div>
         </header>
